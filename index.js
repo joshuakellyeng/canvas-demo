9496c89dbc4dfd83dd0e5d392857838305a1e5d5
Joshua Kelly <joshuakellyeng@gmail.com>
2021-12-21 16:59:32 -0500

    used for loops to animate and create circles

--- index.js
+++ index.js
@@ -62,41 +62,86 @@ console.log(canvas)
 //     c.stroke();
 // }
 
-    let x = 100
-    let y = 100
+// //using the math.random function allows up to randomly place or circle anywhere within the window up loading
+// var x = Math.random() * innerWidth;
+// var y = Math.random() * innerHeight;
 
-    //this variable will determine the directional velocity
-    let dx = 1
-    let dy = 1
+// //this variable will determine the directional velocity
+// var dx = (Math.random() - 0.5) * 8;
+// var dy = (Math.random() - 0.5) * 8;
 
-    //lets create a variable for radius to allow for edge detection
-    let radius = 30
+// //lets create a variable for radius to allow for edge detection
+// var radius = 30
     
-    function animate() {
-        //this function calls itself creating an infinite loop
-        requestAnimationFrame(animate)
-        //this method allows us to clear our frame after every loop
-        c.clearRect(0, 0, innerWidth, innerHeight)
-
-        c.beginPath();
-        c.arc(x,y, radius, 0, Math.PI * 2, false)
-        c.strokeStyle = `red`
-        c.stroke();
-        //using increments to redraw the new circle in a new frame thus giving the illusion of animation
+// circle object
+class Circle {
+    constructor(x, y, dx, dy, radius){
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius
+        //this will be our circle method
+        this.draw = function() {
+            c.beginPath();
+            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+            c.strokeStyle = `red`
+            c.stroke();
+            c.fill()
+            
+        }
+        this.update = function() {
+             //using increments to redraw the new circle in a new frame thus giving the illusion of animation
         //logic in regards to edge detection====================================================
         //this conditional statement essentially states if our x directional velocity + our conditional statement is greater than our innerWidth change the directional value from positive to negative
-        if (x + radius > innerWidth || x - radius < 0) {
-           dx = -dx
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx
+         }
+           //this conditional statement essentially states if our y directional velocity + our conditional statement is greater than our innerWidth change the directional value from positive to negative
+         if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+             this.dy = -this.dy
+         }
+         //dx stands for directional x value
+         this.x += this.dx;
+         //dy stands for directional y value
+         this.y += this.dy;
+         //the size of the increment determines the velocity 
+         this.draw()
         }
-          //this conditional statement essentially states if our y directional velocity + our conditional statement is greater than our innerWidth change the directional value from positive to negative
-        if (y + radius > innerHeight || y - radius < 0) {
-            dy = -dy
-        }
-
-        x+= dx;
-        y+= dy;
-        //the size of the increment determines the velocity 
-        
     }
+}
 
-    animate()
+
+
+var circleArray =[];
+//this for loop creates circle objects and stores them in an array each with their own unique postion and speed
+for(var i = 0; i < 100; i++) {
+
+    //lets create a variable for radius to allow for edge detection
+    var radius = 30
+    //using the math.random function allows up to randomly place or circle anywhere within the window up loading
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+
+    //this variable will determine the directional velocity
+    var dx = (Math.random() - 0.5);
+    var dy = (Math.random() - 0.5);
+
+
+    circleArray.push(new Circle(x,y,dx,dy,radius))
+
+}
+
+
+
+
+function animate() {
+        //this function calls itself creating an infinite loop this is called recursion 
+        requestAnimationFrame(animate);
+        //this method allows us to clear our frame after every loop
+        c.clearRect(0, 0, innerWidth, innerHeight);
+        for(var i = 0; i < circleArray.length; i++){
+            circleArray[i].update();
+        }
+    }
+    animate();
