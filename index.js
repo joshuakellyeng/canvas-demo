415e920198433040e37fefbe83474b3774a28e12
Joshua Kelly <joshuakellyeng@gmail.com>
2021-12-21 12:29:58 -0500

    taking notes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ canvas.height = window.innerHeight;
 //this variable exist as a magic paintbrush altho called context we call it c because we will be using it very often
 let c = canvas.getContext('2d')
 //c.fillRect(x coordinate, y coordinate, width of rect, height oif rect)
+c.fill
 c.fillRect(100, 100, 100, 100)
 c.fillRect(200, 200, 100, 100)
 c.fillRect(300, 100, 100, 100)
@@ -14,3 +15,18 @@ c.fillRect(500, 100, 100, 100)
 
 
 console.log(canvas)
+
+//Drawing lines
+//initiates line drawing
+c.beginPath();
+//Move to takes x and y coordinates to move brush to a specific location
+c.moveTo(100, 300)
+//LineTo draws a line to the indicated x and Y arguments
+c.lineTo(100, 400)
+c.lineTo(200, 400)
+c.lineTo(200, 300)
+c.lineTo(100, 300)
+//stroke Style method lets you input a color value 
+c.strokeStyle = 'red'
+c.stroke();
+c.closePath();
